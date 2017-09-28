5dc126cbf5c6e9fddaeba08e6a72abad92cb1c4a
efrensubdy <efrensubdy@gmail.com>
2017-09-28 09:26:44 -0500

    login changed

--- src/main/resources/static/app/login2/login2.js
+++ src/main/resources/static/app/login2/login2.js
@@ -60,8 +60,8 @@ angular.module('myApp.login2', ['ngRoute'])
         }
       }
 
-      var url2="http://localhost:8080/app/login/contratante/categoria/" + $scope.email +"/" + $scope.password;
-
+      //var url2="http://localhost:8080/app/login/contratante/categoria/" + $scope.email +"/" + $scope.password;
+      var url2="http://ec2-35-163-21-208.us-west-2.compute.amazonaws.com:8080/app/login/contratante/categoria/" + $scope.email +"/" + $scope.password;
       $http.get(url2).then(function(response){
                                            t=response.data;
                                            console.log(response.data);
