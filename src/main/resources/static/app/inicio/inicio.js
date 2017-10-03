144e353a0c28c8b11aa15e0bbd13a976ad29c2b8
efrensubdy <efrensubdy@gmail.com>
2017-10-03 09:30:38 -0500

    Inicio Pro

--- src/main/resources/static/app/inicio/inicio.js
+++ src/main/resources/static/app/inicio/inicio.js
@@ -12,7 +12,11 @@ angular.module('myApp.inicio', ['ngRoute'])
 .controller('inicioCtrl', ['$timeout', '$q', '$scope','$log','$rootScope','$localStorage','$sessionStorage','$location',function($timeout, $q, $scope,$log,$rootScope,$localStorage,$sessionStorage,$location) {
 
 $scope.url="img/Recurso 16.png";
+$scope.url2="img/Recurso 18.png";
+$scope.url3="img/Recurso 20.png"
 $scope.count=0;
+$scope.count2=0;
+$scope.count3=0;
 $scope.a=function(){
 
     $localStorage.banderaCandidato=true;
@@ -38,9 +42,37 @@ $scope.mousem=function(){
   console.log("no soy par");
   $scope.url= "img/Recurso 17.png";
   }
+  $scope.count= $scope.count + 1;
+  }
 
+$scope.mousem2=function(){
 
-$scope.count= $scope.count + 1;
+  if($scope.count2  % 2 == 0 ){
+
+  console.log("Soy par");
+  $scope.url2= "img/Recurso 18.png";
+  }
+  else{
+  console.log("no soy par");
+  $scope.url2= "img/Recurso 19.png";
+  }
+
+$scope.count2= $scope.count2 + 1;
 }
+$scope.mousem3=function(){
+
+  if($scope.count3  % 2 == 0 ){
+
+  console.log("Soy par");
+  $scope.url3= "img/Recurso 20.png";
+  }
+  else{
+  console.log("no soy par");
+  $scope.url3= "img/Recurso 21.png";
+  }
+
+$scope.count3= $scope.count3 + 1;
+}
+
 
 }]);
